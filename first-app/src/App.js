c136ade1ed4bfe20bbbd977665853034e4577757
Cofe1230 <winner1230@gmail.com>
2023-09-01 12:30:07 +0900

    상위 데이터 하위로 연결

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -5,14 +5,46 @@ import Counter from './Counter';
 import Food from './Food';
 import Movie from './Movie';
 import Counter2 from './Counter2';
+import MyDevice from './MyDevice';
 
 
 function App() {
-  
+  const mydataApp = {
+    "myDeviceData" : [
+      {
+        name : "아이폰",
+        ram : "6gb",
+        touch : "yes",
+        face : "yes"
+      },
+      {
+        name : "갤럭시 s22",
+        ram : "8gb",
+        touch : "no",
+        face : "yes"
+      },
+      {
+        name : "갤럭시 z",
+        ram : "6gb",
+        touch : "no",
+        face : "no"
+      },
+      {
+        name : "픽셀5",
+        ram : "6gb",
+        touch : "yes",
+        face : "yes"
+      }
+    ]
+  }
   return( 
     <div>
       <h3>Counter2 클래스 화살표 함수</h3>
       <Counter2/>
+      <hr/>
+      <h3>MyDevice 컴포넌트</h3>
+      <MyDevice mydata = {mydataApp}/>
+      <hr/>
       <Movie title = "영화1"/>
       <Movie title = "영화2"/>
       <Movie title = "영화3"/>
