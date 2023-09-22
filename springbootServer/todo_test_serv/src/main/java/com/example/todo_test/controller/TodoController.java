49ae75d9f091ee6128b7412951b483f456e9641a
Cofe1230 <winner1230@gmail.com>
2023-09-22 17:37:14 +0900

    todo

--- springbootServer/todo_test_serv/src/main/java/com/example/todo_test/controller/TodoController.java
+++ springbootServer/todo_test_serv/src/main/java/com/example/todo_test/controller/TodoController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.todo_test.dto.TodoDto;
 import com.example.todo_test.entity.TodoEntity;
 import com.example.todo_test.service.TodoService;
 
